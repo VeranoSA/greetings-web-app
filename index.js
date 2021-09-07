1a81b0b265dd3315bc8c8076c80788aee80b5dc9
Tebogo Seanego <50863472+VeranoSA@users.noreply.github.com>
2021-09-07 17:49:27 +0200

    Heroku Credentials

--- index.js
+++ index.js
@@ -38,8 +38,7 @@ const pool = new Pool({
     host: 'ec2-44-197-94-126.compute-1.amazonaws.com',
     database: 'd4t968b5v0lopc',
     password: '4453f68486dd6981ce17e604f18a4d0b7dfb16a410de1c7cf0d6e20a22a6d8d4',
-    port: 5432,
-    ssl: true
+    port: 5432
 });
 
 app.use(session({
